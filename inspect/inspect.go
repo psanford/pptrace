fc30b87faa529233598171fa8248140ad53800ee
Peter Sanford <psanford@sanford.io>
2021-12-28 12:57:23 -0600

    Add `inspect args` to show function arguments
    Right now this uses DWARF to get the arguments.
    
    I'd like to also support non-DWARF ELF files that have this
    information in other sections.

--- inspect/inspect.go
+++ inspect/inspect.go
@@ -1,9 +1,11 @@
 package inspect
 
 import (
+	"debug/dwarf"
 	"debug/elf"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
@@ -13,6 +15,7 @@ import (
 
 var (
 	jsonOutput bool
+	allFlag    bool
 )
 
 func Command() *cobra.Command {
@@ -21,10 +24,11 @@ func Command() *cobra.Command {
 		Short: "Inspect a binary or library",
 	}
 
-	cmd.AddCommand(listSymbolsCommand())
-	cmd.AddCommand(listFunctionsCommand())
 	cmd.AddCommand(infoCommand())
 	cmd.AddCommand(listSectionsCommand())
+	cmd.AddCommand(listSymbolsCommand())
+	cmd.AddCommand(listFunctionsCommand())
+	cmd.AddCommand(functionArgsCommand())
 
 	return &cmd
 }
@@ -157,7 +161,7 @@ func listSymbolsAction(cmd *cobra.Command, args []string) {
 
 func listFunctionsCommand() *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "functions <file>",
+		Use:   "functions <file> [filter]",
 		Short: "List functions",
 		Run:   listFunctionsAction,
 	}
@@ -167,9 +171,13 @@ func listFunctionsCommand() *cobra.Command {
 
 func listFunctionsAction(cmd *cobra.Command, args []string) {
 	if len(args) < 1 {
-		log.Fatalf("Usage: functions <file>")
+		log.Fatalf("Usage: functions <file> [filter]")
 	}
 
+	var filterString string
+	if len(args) > 1 {
+		filterString = args[1]
+	}
 	exe, err := elf.Open(args[0])
 	if err != nil {
 		log.Fatalf("Open elf err: %s", err)
@@ -191,6 +199,152 @@ func listFunctionsAction(cmd *cobra.Command, args []string) {
 			continue
 		}
 
-		fmt.Printf("%016x %016x %s\n", sym.Value, sym.Size, sym.Name)
+		if len(filterString) == 0 || strings.Contains(sym.Name, filterString) {
+			fmt.Printf("%016x %016x %s\n", sym.Value, sym.Size, sym.Name)
+		}
 	}
+}
+
+func functionArgsCommand() *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "args <file> [<function-name>|-all]",
+		Short: "Show available function variables",
+		Run:   funcArgsAction,
+	}
+
+	cmd.Flags().BoolVarP(&allFlag, "all", "", false, "Show all functions")
+
+	return &cmd
+}
+
+func funcArgsAction(cmd *cobra.Command, args []string) {
+	if len(args) < 1 {
+		log.Fatalf("Usage: args <file> [<function>|-all]")
+	}
+
+	if len(args) < 2 && !allFlag {
+		log.Fatalf("Usage: args <file> [<function>|-all]")
+	}
+
+	var matchFuncName string
+	if !allFlag {
+		matchFuncName = args[1]
+	}
+
+	exe, err := elf.Open(args[0])
+	if err != nil {
+		log.Fatalf("Open elf err: %s", err)
+	}
+
+	defer exe.Close()
+
+	dwarfInfo, err := exe.DWARF()
+	if err != nil {
+		log.Fatalf("read dwarf err: %s", err)
+	}
+
+	// var inFuncDef bool
+
+	r := dwarfInfo.Reader()
+
+	var (
+		first = true
+		stack = make([]*dwarfNode, 0)
+		root  *dwarfNode
+	)
+
+	for {
+		entry, err := r.Next()
+		if err == io.EOF || entry == nil {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		if first {
+			first = false
+			root = &dwarfNode{
+				entry:    *entry,
+				children: make([]*dwarfNode, 0),
+			}
+			stack = append(stack, root)
+		}
+
+		node := dwarfNode{
+			entry: *entry,
+		}
+
+		// empty node denotes we're at the end of the parent's children
+		if entry.Children == false && entry.Offset == 0 && entry.Tag == 0 {
+			stack = stack[:len(stack)-1]
+			continue
+		}
+
+		parent := stack[len(stack)-1]
+		parent.children = append(parent.children, &node)
+
+		if entry.Children {
+			stack = append(stack, &node)
+		}
+
+	}
+
+	r.Seek(0)
+	for _, pkgs := range root.children {
+		for _, pkgNode := range pkgs.children {
+			// // function definition
+			if pkgNode.entry.Tag == dwarf.TagSubprogram {
+				var matchedName bool
+				for _, field := range pkgNode.entry.Field {
+					if field.Attr == dwarf.AttrName {
+						funcName := field.Val.(string)
+						if strings.Contains(funcName, matchFuncName) {
+							fmt.Printf("%s\n", funcName)
+							matchedName = true
+						}
+					}
+				}
+
+				if !matchedName {
+					continue
+				}
+
+				for _, funcChild := range pkgNode.children {
+					// function argument
+					if funcChild.entry.Tag == dwarf.TagFormalParameter {
+						var (
+							name     string
+							typeName string
+						)
+
+						for _, field := range funcChild.entry.Field {
+							if field.Attr == dwarf.AttrName {
+								name = field.Val.(string)
+							}
+
+							if field.Attr == dwarf.AttrType {
+								r.Seek(field.Val.(dwarf.Offset))
+								typeEntry, err := r.Next()
+								if err != nil {
+									log.Fatal(err)
+								}
+								for i := range typeEntry.Field {
+									if typeEntry.Field[i].Attr == dwarf.AttrName {
+										typeName = typeEntry.Field[i].Val.(string)
+									}
+								}
+							}
+						}
+
+						fmt.Printf("\t%s %s\n", name, typeName)
+					}
+				}
+			}
+		}
+	}
+}
+
+type dwarfNode struct {
+	entry    dwarf.Entry
+	children []*dwarfNode
 }
